389fac15842d7642b55817790eb31b4a7a33d5d9
R M Shahidul Islam <shahedbddev@gmail.com>
2024-08-18 01:05:44 +0600

    + project code added

---
+++ TutorialProject/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace TutorialProject.Models
+{
+    public class Category
+    {
+        public Int64 Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
